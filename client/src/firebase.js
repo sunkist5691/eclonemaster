f4966560e51800694d4c09b8a3f4842c17dad96e
Joon <jong@jongs-mbp.lan>
2021-02-16 15:51:45 -0800

    tda

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,14 +1,17 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDLA8uB6gluRv9vjT8jhIToP-D44wKshJI",
-  authDomain: "ecommerce-af12b.firebaseapp.com",
-  projectId: "ecommerce-af12b",
-  storageBucket: "ecommerce-af12b.appspot.com",
-  messagingSenderId: "650305414125",
-  appId: "1:650305414125:web:bb5ced101ef42b3c45bde4",
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
